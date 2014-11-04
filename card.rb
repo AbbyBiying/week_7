72f20be722062a23f654b35661c16afa851eb97e
Abby BiYing <abbyzhangnyc@gmail.com>
2014-11-03 21:07:10 -0500

    fix card.rb

--- card.rb
+++ card.rb
@@ -1,15 +1,20 @@
-class Card
-  attr_reader :value, :suit
-
-  SUIT = ["C", "D", "H", "S"]
+ class Card
+   attr_reader :card
   
-  VALUE = ["A", 2, 3, 4, 5, 6, 7, 8, 9, "T", "J", "Q", "K"]
+   VALUE = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"]
 
-  def show
-    puts "#{VALUE.sample}#{SUIT.sample}"
+
+   SUIT = ["C", "D", "H", "S"]
+
+   def show
+    VALUE.each do |value|
+      SUIT.each do |suit|
+        puts value + suit 
+      end
+    end 
   end
- 
-end
- card = Card.new
- card.show
 
+end 
+ 
+array = []
+array << Card.new.show 
